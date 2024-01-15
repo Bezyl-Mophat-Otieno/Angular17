ca6aa035c9b29d07bec31f2a94a0851cad18bc3e
Bezyl Mophat Otieno <bezyl.otieno@griffinglobaltech.com>
2024-01-15 07:41:54 +0300

    Added Routing to the Application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { DetailsComponent } from './details/details.component';
+import { HomeComponent } from './home/home.component';
+import { HousingLocationComponent } from './housing-location/housing-location.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: 'details/:id', component: DetailsComponent },
+    { path: 'housing', component: HousingLocationComponent},
+    { path: '', component: HomeComponent },
+];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,11 +3,12 @@ import { HousingLocationComponent } from '../housing-location/housing-location.c
 import { Housinglocation } from '../housinglocation';
 import { CommonModule } from '@angular/common';
 import { HousingService } from '../housing.service';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HousingLocationComponent , CommonModule],
+  imports: [HousingLocationComponent , CommonModule , RouterLink , RouterLinkActive , RouterOutlet],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
